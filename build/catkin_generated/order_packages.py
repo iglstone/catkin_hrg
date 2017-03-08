3bdad46e4b53a6fbad17a759447a860e5758d633
guolong <i690193240@icloud.com>
2017-03-08 15:44:26 +0800

    catkin_make
    cmake the project

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/gl/catkin_hrg/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/gl/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/gl/catkin_ws/devel;/opt/ros/indigo" != "" else []
